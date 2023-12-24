209cb3c3a769cfe5383b06e235c348bbbc88b99c
Stefan Caloian <stefc12341@outlook.com>
2023-12-24 16:20:30 -0500

    added headers and footers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Terminal from './components/Terminal';
+import Footer from "./components/Footer";
+import Header from "./components/Header"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div class="container">
-      <div className="terminal-container">
-        <Terminal />
+      <div className="header">
+        <Header />
       </div>
+      <div class="container">
+        <div className="terminal-container">
+          <Terminal />
+        </div>
+        
+      </div>
+
+      <div className="footer">
+        <Footer />
       </div>
     </div>
   );
